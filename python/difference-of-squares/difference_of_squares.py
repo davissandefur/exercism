6bd3844780372c96d193e3c312246646d92e5161
davissandefur <davis.sandefur@gmail.com>
2015-04-06 10:12:27 -0500

    Made program much simpler with range and list comprehension.

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,18 +1,10 @@
 def square_of_sum(N):
 	""" This function takes a natural number N and squares the sum of the first N numbers """
-	sum = 0
-	while N > 0:
-		sum += N
-		N -= 1
-	return sum**2
+	return sum(range(N + 1))**2
 
 def sum_of_squares(N):
 	""" This function takes a natural number N and returns the sum of the squares of the first N numbers"""
-	sum = 0
-	while N > 0:
-		sum += N**2
-		N -= 1
-	return sum
+	return sum([i**2 for i in range(N + 1)])
 
 def difference(N):
 	""" This fucntion takes a natural number N and returns the difference of squares for the first N."""
