9aae477edce4b5d8480f9dd0e3ea887ac4dbdc42
Davis Sandefur <davis.sandefur@gmail.com>
2015-04-10 14:42:37 -0500

    Second C# problem

---
+++ csharp/bob/Bob.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Bob
+{
+
+    public class Bob
+    {
+        public bool Hey(string phrase)
+        {   
+            if (IsSilence(phrase)){
+                return "Fine. Be that way!";
+            }
+            if (IsQuestion(phrase)){
+                return "Sure.";
+            }
+            if (IsYell(phrase)){
+                return "Whoa, chill out!";
+            }
+            else{
+                return "Whatever.";
+            }
+        }
+        private bool IsSilence(string phrase)
+        {
+            return phrase.Trim() = "";
+        }
+        private bool IsQuestion(string phrase)
+        {
+            return phrase.Trim().EndsWith("?");
+        }
+        private bool IsYell(string phrase)
+        {
+            return phrase.ToUpper() == phrase;
+        }
+    }
+}
