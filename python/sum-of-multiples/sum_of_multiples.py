6b745f70768cfc3ce138a75a5fd08363cb805841
davissandefur <davis.sandefur@gmail.com>
2015-04-09 23:49:48 -0500

    Updated to reflect better understanding of unit test readme.

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,6 +1,5 @@
-def sum_of_multiples(factor=6, multiples=[3, 5]):
-    """ This function returns the sum of all multiples of 3 and 5 up to but not including (factor). Factor is
-    initalized as 6 so that if not factor is given, it uses the list [3,5], per instructions"""
-    if multiples[0] == 0:
-        multiples = multiples[1:]
-    return sum(value for value in range(factor) if any(value % multiple == 0 for multiple in multiples))
+def sum_of_multiples(number, factors=[3, 5]):
+    """ This function returns the sum of all multiples of 3 and 5 up to but not including (number). Factors is initialized so it uses the list [3,5], per instructions"""
+    if factors[0] == 0:
+        factors = factors[1:]
+    return sum(value for value in range(number) if any(value % factor == 0 for factor in factors))
